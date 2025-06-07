fd6733f19aaca4eb400b8fde8d31ff86d74fd356
Laura Chipman <laurachipman66@gmail.com>
2025-06-07 12:57:20 -0400

    Added vercel analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,15 @@
 import '@/styles/globals.css'; // or '../styles/globals.css'
 import React from 'react';
 import 'tailwindcss/tailwind.css'
+import { Analytics } from "@vercel/analytics/next"
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+export default function MyApp({ Component, pageProps }) {
+  return (
+      <>
+        <Component {...pageProps} />
+        <Analytics />
+      </>
+  )
 }
 
 export default MyApp;
